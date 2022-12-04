f798e34aae9dc9936f6cf48dd36efdb5e8462285
gelinagashi <gashigelina@gmail.com>
2022-12-04 16:25:22 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -121,3 +121,9 @@ namespace AES
             //but it is a must ??
             
              finalMatrix = new int[4, 4];
+             
+              for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    finalMatrix[i,j] = roundedMatrix[j,i];
+            }
