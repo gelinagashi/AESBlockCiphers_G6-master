227ed7afff7d59d362e6937035bdac4b097428e1
gelinagashi <gashigelina@gmail.com>
2022-12-04 15:43:29 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,3 +8,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+namespace AES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
