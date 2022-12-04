1578219c3f927f5245ec064d523dff95537028a6
HQendresa <119263143+HQendresa@users.noreply.github.com>
2022-12-04 16:14:40 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -60,3 +60,10 @@ namespace AES
 
                 xPos++;
             }
+             /*
+            initialMatrix = new int[4, 4] { { 0x32, 0x88, 0x31, 0xe0},
+                                            { 0x43, 0x5a, 0x31, 0x37},
+                                            { 0xf6, 0x30, 0x98, 0x07},
+                                            { 0xa8, 0x8d, 0xa2, 0x34}};
+                                                                          */
+
